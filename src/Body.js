626b662a5975f3651d73701e77711135f25f3b83
VasistaK <vasistakanderi81@gmail.com>
2024-07-01 18:45:48 +0530

    Updated css and Js

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,5 @@
 import { useEffect, useState } from "react";
 
-import {FaTemperatureArrowDown,FaWind,FaTemperatureArrowUp,FaTemperatureThreeQuarters } from "react-icons/fa6";
-
-import { FaLocationDot } from "react-icons/fa6";
-
-
 import useOnline from "./useOnline";
 
 export const Body = ()=>{
@@ -13,13 +8,20 @@ const isOnline = useOnline();
 
 const [info,setInfo] = useState("");
 
-const [search,setSearch] = useState("");
+const [city] = useState("dallas")
+
+const [search,setSearch] = useState();
+
+
+
+
+
 
    async function getData (){
 
-    if(search){
+    if(search||city){
 
-const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=19e679057b9bcb475f2b0fdccb53c134`)
+const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search||city}&units=metric&appid=19e679057b9bcb475f2b0fdccb53c134`)
 
 const json  = await data.json();
 
@@ -28,40 +30,77 @@ setInfo(json);
 console.clear()
 
 }
+
 }
 
-//weather[0].description
+
    useEffect(()=>{
-
     getData();
-
 },[search])
 
 return (isOnline)? (
   
 <div className="AppPage">
 
+
+<div className="search-box">
+    <input type="search"  value={search} placeholder="Enter Your Location" onChange={(e)=>{setSearch(e.target.value)}}/>
+</div>
+
 <div className="weathercard">
 
 
-<input type="search"  value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
-
 {info.message === "city not found"? <h3 className="NoData">No data found</h3>:
+
 <>
 
-<div className="city"><FaLocationDot />{info?.name} {(info?.sys?.country)}</div>
+<div className="city">
+{info?.name} {(info?.sys?.country)} 
+</div>
 
-<div className="temp"><FaWind />{info?.wind?.speed} MPH</div>
+<div className="temp">
+   {info?.main?.temp}℃
+</div>
 
-<div className="description">{info.weather?.map(x=>x.description)}</div>
 
-<div className="temp"><FaTemperatureThreeQuarters/>{info?.main?.temp}℃</div>
+
+<div className="description">
+    {info.weather?.map(x=>x.description)}
+</div>
 
 <div className="temp-spec">
 
-<div className="min-temp"><FaTemperatureArrowDown/> {info?.main?.temp_min}℃</div> <div className="temp-max"><FaTemperatureArrowUp/> {info?.main?.temp_max}℃</div>
+<div className="wind">
+   {info?.wind?.speed} MPH<p>WindSpeed</p>
+</div>
+
+<div className="min-temp">
+    {info?.main?.temp_min}℃
+    <p>Min_Temp</p>
+</div> 
+
+<div className="temp-max">
+        {info?.main?.temp_max}℃
+        <p>Max_Temp</p>
+</div>
+
+<div className="humidity">
+        {info?.main?.humidity}%
+        <p>Humidity</p>
+</div>
+
+<div className="sealevel">
+        {info?.main?.sea_level}FT
+        <p>sea_level</p>
+</div>
+
+<div className="sealevel">
+        {info?.main?.feels_like}℃
+        <p>feels_like</p>
+</div>
 
 </div>
+
 </>
 
 }
