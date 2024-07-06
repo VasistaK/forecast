6db5c40081d008e33c10f63467ed402a91c049b6
VasistaK <vasistakanderi81@gmail.com>
2024-07-06 21:56:44 +0530

    updated media queries

--- src/Body.js
+++ src/Body.js
@@ -12,6 +12,9 @@ const [city] = useState("Delhi")
 
 const [search,setSearch] = useState("");
 
+const [weather,setWeather] = useState("https://cdn3.iconfinder.com/data/icons/agriculture-196/64/934.png")
+
+
     async function handleClick(){
     
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=19e679057b9bcb475f2b0fdccb53c134`)
@@ -22,103 +25,80 @@ const [search,setSearch] = useState("");
     
     console.log(json)
     
-    console.clear()
+    // console.clear()
     
-    }
-       
+
+
+    }   
+
+
+      const Icon = ()=>{
+      if(info?.weather?.map(x=>x.main) =='Clouds'){
+        setWeather("https://cdn4.iconfinder.com/data/icons/weather-129/64/weather-2-64.png");
+        console.log("cloudy");
+      }
+      else if(info?.weather?.map(x=>x.main)=='Haze'){
+        setWeather("https://cdn4.iconfinder.com/data/icons/linecon/512/cloud-64.png");
+        console.log("Haze");
+      }
+      else if(info?.weather?.map(x=>x.main)=='Clear'){
+        setWeather("https://cdn3.iconfinder.com/data/icons/agriculture-196/64/934.png");
+        console.log("clear");
+      }
+      else if(info?.weather?.map(x=>x.main) == 'Rain'){
+        setWeather("https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_7-64.png");
+        console.log("Rain");
+      }}
+
+
+      useEffect(()=>{Icon()},[handleClick])
+
+
         useEffect(()=>async function Data (){
+
             const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${"Delhi"}&appid=19e679057b9bcb475f2b0fdccb53c134`)
             
             const json  = await data.json();
             
             setInfo(json);
-            
-            console.log(json)
-            
+
             console.clear()
-            
+
             },[null])
 
-
 return (isOnline)? (
-  
-<div className="AppPage">
 
-<div className="search-box">
-    <input type="search" value={search} placeholder="Search" onChange={(e)=>{setSearch(e.target.value)}}/>
-    <button class="search-btn"  onClick={handleClick}>
-        <img src="https://cdn1.iconfinder.com/data/icons/jumpicon-basic-ui-glyph-1/32/-_Magnifier-Search-Zoom--64.png"/>
-    </button>
-    
-</div>
-
-<div className="weathercard">
-
-<div></div>
-{(info.message === 'city not found' ) ?<h3 className="NoData">No Location Found</h3>:
+<>
+<div className="search-container">
+    <input placeholder="Enter Location" onChange={(e)=>{setSearch(e.target.value)}}/>
+     <button onClick={handleClick}>
+        Search
+     </button>
+     </div>
+     {(info.message === 'city not found')?<h3>No Data Found</h3>:
+     <>
+<div className="city-temperature-container">
 <div>
-<div className="city-temp">
-  <div>  
- <div class="city-country">   
-   {info?.name} {(info?.sys?.country)}
-</div>
-<div class="temp">
-{info?.main?.temp}℉
-</div>
-</div>
-
-<div className="description">
-<div >
-    {info.weather?.map(x=>x.description)}
-</div>
+<p>{info?.name}<img src={weather} alt="icon"/></p>
+<p>{info?.main?.temp}℉</p>
 
 </div>
-
+<div className="weather-description">
+  {info?.weather?.map(x=>x.description)}
+</div>
 </div>
 
-
-<div className="climate-info">
-
-<div className="wind">
-   {info?.wind?.speed} MPH<p>WindSpeed</p>
+<div className="weather-now">
+<div>{info?.main?.feels_like}℉<p>Real_Feel</p></div>
+<div>{info?.main?.humidity}%<p>Humidity</p></div>
+<div>{info?.main?.sea_level}FT<p>Sea_level</p></div>
+<div>{info?.main?.temp_max}℉<p>Temp_Max</p></div>
+<div>{info?.main?.temp_min}℉<p>Temp_Min</p></div>
+<div>{info?.wind?.speed}MPH<p>Wind_Speed</p></div>
 </div>
-
-<div className="min-temp">
-    {info?.main?.temp_min}℉
-     <p>Min_Temp</p>
-</div> 
-
-<div className="max-temp">
-        {info?.main?.temp_max}℉
-         <p>Max_Temp</p>
-</div>
-
-<div className="humidity">
-        {info?.main?.humidity}%
-        <p>Humidity</p>
-</div>
-
-<div className="sealevel">
-        {info?.main?.sea_level}FT
-        <p>sea_level</p>
-</div>
-
-<div className="feels-like">
-        {info?.main?.feels_like}℉
-        <p>feels_like</p>
-</div>
-
-</div>
-
-</div>
-
-}
-
-
-
-</div>
-
-</div>
+   <div className="Title">OverDrop</div>
+   </>
+}</>
 )
  :<h3>No Internet Connection Found</h3>     
-}
+    }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import './styles/general.css'
-import './styles/Search.css'
 import App from './App';
 
 
